599849387618db881f76159b1287354c94712f50
rokas <rokasra@gmail.com>
2022-03-22 10:08:09 +0200

    timeOut cleaned

--- js/timeOut.js
+++ js/timeOut.js
@@ -1,21 +1,18 @@
+//setTimeout be argumento
+
 const h1El = document.querySelector("h1");
 function changeH1() {
   h1El.textContent = "It was changed";
 }
-
-// changeH1();
-
 //setTimeout (kokia fn vykdisim, uz kiek laiko ms)
 setTimeout(changeH1, 3000);
-
-//____________________//
-
+//___________________________________//
+//
+//setTimeout su argumentu
 const h2El = document.querySelector("h2");
 function changeH2(newTitle) {
-  h1El.textContent = newTitle;
+  h2El.textContent = newTitle;
 }
-
-// changeH1();
-
 //setTimeout (kokia fn vykdisim, uz kiek laiko ms)
-setTimeout(() => changeH2("hello"));
+setTimeout(() => changeH2("hello"), 4000);
+setTimeout(changeH1, 4000, "Hello");
